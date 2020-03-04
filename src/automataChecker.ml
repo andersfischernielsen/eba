4086b295904baa0874df13f66300e7042a31a954
Anders Fischer-Nielsen <andersfischern@me.com>
2020-03-04 09:31:25 +0100

    Clean up comments, unused code

--- src/automataChecker.ml
+++ src/automataChecker.ml
@@ -14,7 +14,6 @@ module type AutomataSpec = sig
 
 	(** Checker's internal state, eg. memory regions to track. *)
 	type checker_state = {
-		(* previous_state : state; *)
 		current_state: state;
 		trace: step list;
 		matches: step list;
@@ -23,12 +22,6 @@ module type AutomataSpec = sig
 
 	(** States *)
 	val initial_state : checker_state
-	(* val state_to_string : state -> string *)
-	(* val checker_state_to_string : checker_state -> string *)
-	(* val copy_state : checker_state -> checker_state *)
-	(* val reset_state : checker_state -> checker_state *)
-	(* val init_state : state -> step list -> state -> step list -> checker_state *)
-	(* val compare_states : state -> state -> int *)
 	val compare_checker_states : region * checker_state -> region * checker_state -> int
 	val is_accepting : checker_state -> bool
 	val transition_labels : mem_kind list
@@ -80,8 +73,11 @@ module Make (A : AutomataSpec) : S = struct
 		| _ -> None
 
 	let rec explore_paths path map = 
-		match path() with
+		let p = path() in
+		match p with
 		| Seq(step, remaining) -> 
+			let shown_step = pp_step step |> PP.to_string in 
+			let shown_path = remaining() in
 			let apply_transition effect map = 
 				let region = get_region effect in 
 					match region with 
@@ -116,13 +112,7 @@ module Make (A : AutomataSpec) : S = struct
 			let true_branch = explore_paths true_path map in
 			let false_branch = explore_paths false_path map in 
 			Map.union true_branch false_branch
-		| _ -> map
-
-	(* 
-	let product initials transitions input = 
-		match initials, transitions with 
-		| (a, a2), (t1, t2) -> (t1 a input, t2 a2 input)
- 	*)
+		| Nil -> map
 	
 	let check file declaration =
 		let variable_info = Cil.(declaration.svar) in
@@ -131,26 +121,8 @@ module Make (A : AutomataSpec) : S = struct
 		let results = explore_paths path_tree Map.empty in 
 		let states = Map.values results in
 		let matches = Enum.fold (fun acc m -> (List.filter A.is_accepting m) @ acc) [] states in
-		let pp = List.map (fun m -> A.pp_checker_state m) matches in
+		let matches_reversed = List.rev matches in 
+		let pp = List.map (fun m -> A.pp_checker_state m) matches_reversed in
 		let pp_list = List.map (fun m -> PP.to_string m) pp in
 		L.of_list pp_list
 end
-
-	(** Might be needed later for duplicate elimination
-	let same_loc (_,s1,_,_) (_,s2,_,_) = Cil.compareLoc s1.sloc s2.sloc = 0
-	let cmp_match (_,s1,p1,_) (_,s2,p2,_) :int =
-		let l1 = s1.sloc in
-		let l2 = s2.sloc in
-		let lc = Cil.compareLoc l1 l2 in
-		(* Order reversed so that L.unique_eq will take the simplest match *)
-		if lc = 0
-		then
-			let pc = Int.compare (List.length p1) (List.length p2) in
-			if pc = 0
-			then -(compare p1 p2)
-			else -pc
-		else -lc 
-
-	(* Remove redundant traces keeping the shortest one (wrt [cmp_match]). *)
-	let nodup = L.(unique_eq ~eq:same_loc % (sort ~cmp:cmp_match)) 
-	**)
--- test/double-unlock.c
+++ test/double-unlock.c
@@ -13,10 +13,6 @@ void double_unlock(int flag)
 	else {
 		_spin_unlock(&lock2);
 	}
-
-	while (flag == 1) {
-		_spin_unlock(&lock3);
-	}
 }
 
 int main()
