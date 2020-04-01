75255db748ea4c8bdb12c31b81b2f11db52cef7c
Anders Fischer-Nielsen <andersfischern@me.com>
2020-04-01 13:39:55 +0200

    Add docker invocation

--- scripts/docker-eval/make-and-evaluate.sh
+++ scripts/docker-eval/make-and-evaluate.sh
@@ -3,11 +3,11 @@ if [ ! -d "$(pwd)/linux" ]; then
 fi
 
 echo "Running docker container.." 
-# docker run  --env "HASH=$1" --env "FILE=$2" -v "$(pwd)/linux":/linux ubuntu-linux
+docker run  --env "HASH=$1" --env "FILE=$2" -v "$(pwd)/linux":/linux ubuntu-linux
 
 echo "Removing output not supported by EBA.."
 sed -i.bak "/^_Static_assert/ d" "linux/$2" && rm "linux/$2.bak"
 sed -i.bak "s/asm __inline volatile/asm volatile/g" "linux/$2" && rm "linux/$2.bak"
 
-echo "Running EBA"
+echo "Running EBA.."
 ./../../bin/eba --dUa "linux/$2"
