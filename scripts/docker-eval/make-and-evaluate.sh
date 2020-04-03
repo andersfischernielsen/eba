219fe63d0dea3ba6440bea6b951d7265f8894d49
Anders Fischer-Nielsen <andersfischern@me.com>
2020-04-03 15:14:19 +0200

    Streamline docker evaluation script, remove script-specific .gitignore

--- scripts/docker-eval/make-and-evaluate.sh
+++ scripts/docker-eval/make-and-evaluate.sh
@@ -1,7 +1,15 @@
 echo "Running docker container.." 
 docker run  -v "$(pwd)/$1":"/linux/$(dirname "$2")" --env "HASH=$1" --env "FILE=$2" ubuntu-linux
 
-find "$1/" ! -name "$(basename "$2")" -type f -exec rm -f {} +
+echo "Cleaning up.."
+output_file=$(basename $2)
+original_file=${output_file%.i}.c
+cp $1/$output_file $output_file
+cp $1/$original_file $original_file
+rm -r $1 
+mkdir $1
+mv $output_file $1/$output_file
+mv $original_file $1/$original_file
 
 echo "Running EBA.."
 ./../../bin/eba --loop-limit=1 --branch-limit=1 --dUa "$1/$(basename "$2")" | tee "$1/$1.txt"
