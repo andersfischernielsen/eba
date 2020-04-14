c63e302b020b2e4346468bc0dd315686565a0d17
Anders Fischer-Nielsen <andersfischern@me.com>
2020-04-14 12:17:14 +0200

    Clean up monitor automaton, add use of inlining, only non-static function exploration

--- src/automataChecker.ml
+++ src/automataChecker.ml
@@ -6,6 +6,7 @@ open Type
 open Abs
 open PathTree
 open Effects
+open PathTree
 
 module type AutomataSpec = sig
 	(** A name to identify the checker *)
@@ -17,21 +18,17 @@ module type AutomataSpec = sig
 		current_state: state;
 		trace: step list;
 		matches: step list;
-		id: int;
 	}
 
 	(** States *)
 	val initial_state : checker_state
-	val compare_checker_states : region * checker_state -> region * checker_state -> int
 	val is_accepting : checker_state -> bool
 	val transition_labels : mem_kind list
-	val pp_checker_state : checker_state -> SmartPrint.t
+	val pp_checker_state : checker_state -> name -> SmartPrint.t
+	val checker_state_to_string : checker_state -> string
 
 	(** Test *)
 	val transition : checker_state -> Effects.e -> step -> checker_state
-
-	(** Bug data *)
-	type bug
 end
 
 module type S = sig
@@ -39,13 +36,6 @@ module type S = sig
 end
 
 module Make (A : AutomataSpec) : S = struct
-	module RegionMonitorKey = 
-	struct
-		type t = region * A.checker_state
-		let compare = fun f s -> A.compare_checker_states f s
-	end
-	module RegionMonitorMap = Map.Make(RegionMonitorKey)
-
 	let is_in_transition_labels effect = 
 		match effect with 
 		| Mem(kind, _) -> List.mem kind A.transition_labels
@@ -60,7 +50,7 @@ module Make (A : AutomataSpec) : S = struct
 				aux [] [] l in
 		match l with 
 		| [] -> []
-		| hd::[] -> [[hd]]
+		| [hd] -> [[hd]]
 		| hd::tl -> List.fold_left (fun acc p -> acc @ insert_all_positions hd p) [] (permute tl)
 
 	let get_region e = 
@@ -70,57 +60,77 @@ module Make (A : AutomataSpec) : S = struct
 			since it doesn't contain relevant information. *)
 		| _ -> None
 
-	let rec explore_paths path map = 
+	let print_map m s = 
+		if Map.is_empty m then Format.printf "%s %s" s "None\n"
+		else
+			let gen_names ss = List.fold_right (fun s acc -> Format.sprintf "%s " (A.checker_state_to_string s) ^ acc ) ss "" in
+			Format.printf "%s\n" s;
+			BatMap.iter (fun k v -> Format.printf "%s: %s" (Region.pp k |> PP.to_string) (v |> gen_names) ) m;
+			Format.printf "%s" "\n"
+
+	let rec explore_paths (func:AFun.t) path map (should_inline:bool)= 
 		let p = path() in
 		match p with
 		| Seq(step, remaining) -> 
-			let shown_step = pp_step step |> PP.to_string in 
-			let shown_path = remaining() in
-			let apply_transition effect map = 
+			let apply_transition effect map_to_add_to = 
 				let region = get_region effect in 
 				match region with 
 				| Some r -> 
 					(* Find the previous result if present, then determine new checker_state. *)
-					let result = Map.find_default [A.initial_state] r map in 
-					let m = List.fold_left (fun acc s -> A.transition s effect step :: acc) [] result in
-					Map.add r m map
-				| None -> map
+					let result = Map.find_default [A.initial_state] r map_to_add_to in 
+					(* Format.printf "%s" "Hit innermost map\n"; *)
+					let applied = List.map (fun s -> (A.transition s effect step)) result in
+					(* Format.printf "%s" "Exited innermost map\n"; *)
+					Map.add r applied map_to_add_to
+				| None -> map_to_add_to
 			in
 
-			let input = EffectSet.filter is_in_transition_labels step.effs.may |> EffectSet.to_list in
+			
+			let input = EffectSet.filter is_in_transition_labels step.effs.must |> EffectSet.to_list in
 			(* Skip step if the effects are uninteresting *)
 			if List.is_empty input 
-			then explore_paths remaining map 
+			then explore_paths func remaining map true
 			else 
+				let inlined_result = if not should_inline then map else 
+					let inlined = inline func step in
+					match inlined with
+					| Some (_, t) -> explore_paths func t map false
+					| None -> map
+				in
+
 				(* 	Find all permutations of effects e.g. {{lock, unlock} -> {{lock, unlock}, {unlock, lock}} 
 					in order to evaluate all effect orders. *)
 				let permutations = permute input in 
-				let map = List.fold_left (fun map effects ->
+				let changed_map = List.fold_right (fun effects map_to_change ->
 						(* 	For each effect in a given permutation, apply the transition function, 
 							and add the result to the (region, checker_state) -> [checker_state] map. 
 							
 							This expresses that a given region has multiple state machines monitoring it 
 							if multiple evaluation orders are possible for the effects of that region. *)
-						List.fold_left (fun map effect -> apply_transition effect map) map effects
-					) map permutations in
-				explore_paths remaining map
+						List.fold_right (fun effect acc -> apply_transition effect acc) effects map_to_change
+					) permutations inlined_result in
+				explore_paths func remaining changed_map true
 		| Assume(_, _, remaining) -> 
-			explore_paths remaining map
+			explore_paths func remaining map true
 		| If(true_path, false_path) -> 
-			let true_branch = explore_paths true_path map in
-			let false_branch = explore_paths false_path map in 
-			Map.union true_branch false_branch
+			let true_branch = explore_paths func true_path map true in
+			let false_branch = explore_paths func false_path map true in 
+			let union = Map.union true_branch false_branch in 
+			union
 		| Nil -> map
-	
+
 	let check file declaration =
 		let variable_info = Cil.(declaration.svar) in
-		let _, global_function = Option.get(AFile.find_fun file variable_info) in
-		let path_tree = paths_of global_function in
-		let results = explore_paths path_tree Map.empty in 
-		let states = Map.values results in
-		let matches = Enum.fold (fun acc m -> (List.filter A.is_accepting m) @ acc) [] states in
-		let matches_reversed = List.rev matches in 
-		let pp = List.map (fun m -> A.pp_checker_state m) matches_reversed in
-		let pp_list = List.map (fun m -> PP.to_string m) pp in
-		L.of_list pp_list
+		match variable_info.vstorage with | Static -> L.of_list []
+		| _ ->
+			let _, global_function = Option.get(AFile.find_fun file variable_info) in
+			let path_tree = paths_of global_function in
+			let results = explore_paths global_function path_tree Map.empty true in 
+			let states = Map.values results in
+			let matches = Enum.fold (fun acc m -> (List.filter A.is_accepting m) @ acc) [] states in
+			let matches_reversed = List.rev matches in 
+			let function_name = Cil.(variable_info.vname) in 
+			let pp = List.map (fun m -> A.pp_checker_state m function_name) matches_reversed in
+			let pp_list = List.map (fun m -> PP.to_string m) pp in
+			L.of_list pp_list
 end
--- src/checkAutomataDoubleUnlock.ml
+++ src/checkAutomataDoubleUnlock.ml
@@ -9,7 +9,6 @@ open Random
 module L = LazyList
 
 module AutomataSpec = struct
-	(* TODO: Remove "of Region.t" since it is technically unused *)
 	type state = 
 		| Initial
 		| Locked
@@ -24,59 +23,26 @@ module AutomataSpec = struct
 		current_state: state;
 		trace: step list;
 		matches: step list;
-		id: int;
 	}
 
 	let state_to_string state = 
 		let open PP in
 		let st = match state with 
-		| Initial -> words "Initial"
-        | Locked 	-> words "Locked"
+        | Initial 	-> words "Initial"
+		| Locked 	-> words "Locked"
         | Unlocked -> words "Unlocked"
         | Error e -> words "Error" ++ brackets (Effects.pp_e e) in
 		st |> PP.to_string
 
 	let checker_state_to_string state = 
-		Format.sprintf "{\n  id=%i\n  current_state=%s\n}\n" 
-			(state.id) (state_to_string state.current_state)
+		Format.sprintf "{ current_state=%s }" (state_to_string state.current_state)
 
 	let initial_state = 
-		let init = Initial in
-		let r = Random.int 1000000 in
 		{ 
-			current_state = init; 
+			current_state = Unlocked; 
 			trace = []; 
 			matches = [];
-			id = r;
 		}
-	
-	(* 
-	let copy_state state = 
-		let r = Random.int 1000000 in
-		let new_state = { 
-			current_state = state.current_state; 
-			trace = state.trace; 
-			matches = state.matches;
-			id = r;
-		} in 
-		(* Format.printf "%s" (checker_state_to_string new_state); *)
-		new_state 
-	*)
-
-	(* 
-	let reset_state state = 
-		let s = match state.current_state with
-			| Unlocked _ -> state.current_state
-			| _ -> Initial in
-		let new_state = {
-			current_state = s; 
-			trace = [];
-			matches = [];
-			id = Random.int 1000000;
-		} in
-		(* Format.printf "%s" (checker_state_to_string new_state); *)
-		new_state 
-	*)
 
 	let with_previous state _new step = 
 		let matches = match _new with | Error _ -> step::state.matches | _ -> state.matches in
@@ -84,30 +50,23 @@ module AutomataSpec = struct
 			trace=step::state.trace; 
 			current_state=_new; 
 			matches=matches;
-			id=state.id
 		} in
-		(* Format.printf "%s" (checker_state_to_string new_state); *)
 		new_state
-		
-	(** Test *)
-	let is_same_region f s = 
-		let compare = Region.compare f s in 
-		compare = 0
-
+				
     let transition previous input step = 
-		let next new_current = with_previous previous new_current step in
+		let next new_state = with_previous previous new_state step in
 		let previous_state = previous.current_state in 
 		match previous_state with 
-        | Initial -> 
+		| Initial ->
 			(match input with 
 			| Mem(Lock, _)		-> next Locked
 			| Mem(Unlock, _)	-> next Unlocked
-			| _					-> next previous_state
+			| _         		-> next previous_state
 			)
 		| Unlocked ->
 			(match input with 
-			| Mem(Lock, _)		-> next Locked
-			| Mem(Unlock, _)	-> next (Error input)
+			| Mem(Lock, _)		-> (*pp_e input |> PP.to_string |> Format.printf "%s\t\t Unlocked -> Locked\n";*) next Locked
+			| Mem(Unlock, _)	-> (*pp_e input |> PP.to_string |> Format.printf "%s\t\t Unlocked -> Error\n";*) next (Error input)
 			| _         		-> next previous_state
 			)
         | Locked ->
@@ -122,20 +81,7 @@ module AutomataSpec = struct
 		| Error _ 	-> true
 		| _ 		-> false
 
-	let compare_states first second =
-		match first, second with
-		| Initial, Initial 		-> true
-		| Locked, Locked 		-> true
-		| Unlocked, Unlocked	-> true
-		| Error fe, Error se	-> fe =. se
-		| _						-> false
-
-	let compare_checker_states first second = 
-		if compare_states (snd first).current_state (snd second).current_state
-		then Region.compare (fst first) (fst second)
-		else -1
-
-	let pp_checker_state (state:checker_state) = 
+	let pp_checker_state (state:checker_state) function_name = 
 		let open PP in 
 		let matches = List.rev state.matches in 
 		let trace = List.rev state.trace in 
@@ -143,12 +89,9 @@ module AutomataSpec = struct
 		let trace_locations = List.fold_left (fun acc m -> acc ++ Utils.Location.pp m.sloc ++ words (string_of_step m) + newline) newline trace in
 		
 		brackets (!^ name) + newline + newline
-		++ words "at:"
-		(* ++ words (Printf.sprintf "%d" state.id) *)
-		++ match_locations + newline
+		++ words "in: " + newline + (!^ function_name) + newline
+		++ words "at:" ++ match_locations + newline
 		++ words "trace:" ++ trace_locations + newline
-
-	type bug = Region.t
 end
 
 module Checker = AutomataChecker.Make(AutomataSpec)
